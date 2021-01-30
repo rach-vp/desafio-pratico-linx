3045b6686e685e9b17963b09ce895b4b400bbfd4
Raquel Picanço <picanco.raquel@gmail.com>
2021-01-29 21:05:53 -0400

    Set connection with MongoDB

--- Linx_Dev_Fullstack_JR/index.js
+++ Linx_Dev_Fullstack_JR/index.js
@@ -1,5 +1,20 @@
 const customExpress = require('./config/customExpress');
+const client = require('./infra/connection');
 
-const app = customExpress();
-const port = 3000;
-app.listen(port, () => console.log(`Server listening port ${port}`));
+async function run() {
+  try {
+    await client.connect(
+      (error, client) => error ? console.log(error) : console.log(`Successfully connected to DB ${client.s.options.dbName}`)
+    );
+
+    // const database = client.db('e-commerce');
+    // const collection = database.collection('catalog');
+    const app = customExpress();
+    const port = 3000;
+    app.listen(port, () => console.log(`Server listening port ${port}`));
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
